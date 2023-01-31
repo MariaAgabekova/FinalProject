fef85938418f6830c3018e19327b85b297fec520
Maria Agabekova <m312102@bk.ru>
2023-02-01 02:48:23 +0600

    Add FormingArrayOfThreeOrFewerSymbols

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] array = new string[] { "Hello", "2", "world", ":-)" };// ["1234", "1567", "-2", "computer science"],["Russia", "Denmark", "Kazan"]
+string[] newArray = new string[array.Length];
+
+void FormingArrayOfThreeOrFewerSymbols(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    }
+}
